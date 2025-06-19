1914518c3e41554c254d2ff3923040c9cfbbf6ba
Ramazan G. <rgaraisayev@gmail.com>
2025-06-20 01:32:10 +0400

    Add server configuration to Vite for port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
